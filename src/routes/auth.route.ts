408a1d5ec8c33e862e19d2026b84ce7191c4c605
Laxmiikhanal <pujakhanal633@gmail.com>
2026-01-05 10:55:02 +0545

    Add user authentication routes and role model

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 
-let authController = new AuthController();
 const router = Router();
+const authController = new AuthController();
 
-router.post("/register", authController.register)
-router.post("/login", authController.login)
-// add remaning routes like login, logout, etc.
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 export default router;
